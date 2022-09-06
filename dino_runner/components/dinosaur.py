8abe183496d5fc54c2c8e1f7a32bf3b8f165a177
David Camacho C <112898884+DavDevCC@users.noreply.github.com>
2022-09-05 21:24:50 -0400

    TAREA CLASE1
    implementacion metodo duck en dino

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import DUCKING, RUNNING, JUMPING
 
 
 class Dinosaur:
@@ -24,6 +24,8 @@ class Dinosaur:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -31,6 +33,11 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        if user_input[pygame.K_DOWN] and not self.dino_jump :
+            self.dino_run = False
+            self.dino_duck = True
+        
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -53,7 +60,12 @@ class Dinosaur:
             self.jump_vel = self.JUMP_VEL
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS + 30
+        self.step_index += 1
+
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
