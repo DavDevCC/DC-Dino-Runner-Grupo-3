2aec2d542818178e171e6d9f63150300c51fd34a
David Camacho C <112898884+DavDevCC@users.noreply.github.com>
2022-09-07 21:07:18 -0400

    codigo clase 3 trainer()

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -4,20 +4,20 @@ from dino_runner.components.obstacles.obstacle import Obstacle
 from dino_runner.utils.constants import BIRD
 
 class Bird(Obstacle):
-    def __init__ (self ):
+    def __init__ (self):
         self.type = 0  
         self.flutter = 0  #aleteo/cambiar imagenes
-        super().__init__ ( BIRD, self.type ) #sacar rect
-        self.rect.y = random.randint (100, 250) 
+        super().__init__ (BIRD, self.type) #sacar rect
+        self.rect.y = random.randint(200, 320) 
         
 
-    def update(self , game_speed , obstacles ) :
-        self.rect.x -= game_speed
-        self.obstale_to_draw = BIRD[0] if self.flutter < 10 else BIRD[1]
+    def update(self, game_speed, obstacles):
+        
+        self.obstacle_to_draw = BIRD[0] if self.flutter < 10 else BIRD[1]
         self.flutter += 1
         if self.flutter >= 20:
             self.flutter = 0    
-        if self.rect.x < -self.rect.width :
-            obstacles.pop()
+        super().update(game_speed, obstacles)
         
+
     
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -6,19 +6,24 @@ from dino_runner.components.obstacles.cactus import Cactus
 
 
 class ObstacleManager:
-    def __init__ (self) :
+    def __init__(self):
         self.obstacles = []   
       
-    def update(self, game ) :
-        if len (self.obstacles ) == 0 :
+    def update(self, game):
+        if len (self.obstacles) == 0:
              self.obstacle_type_list = [Bird(), Cactus()]
              self.obstacles.append(random.choice(self.obstacle_type_list))
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 game.playing = False
+                game.death_count += 1
+                break
 
-    def draw (self, screen) :
-        for obstacle in self.obstacles :
+    def draw(self, screen):
+        for obstacle in self.obstacles:
                 obstacle.draw(screen) 
+    
+    def reset_obstacles(self):###############
+        self.obstacles = []
             
